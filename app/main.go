ccf5c184516851ced858f7d0e8c27eaa5c3aa475
Dat Nguyen <dat.nguyen@silentium.io>
2025-05-01 13:27:16 +0700

    feat: send correlation ID

--- app/main.go
+++ app/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Connection accepted", conn.RemoteAddr())
+	defer conn.Close()
+	resp := make([]byte, 8)
+	binary.BigEndian.PutUint32(resp[0:4], uint32(0))
+	binary.BigEndian.PutUint32(resp[4:8], uint32(7))
+	_, err = conn.Write(resp)
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
 		os.Exit(1)
 	}
 }
